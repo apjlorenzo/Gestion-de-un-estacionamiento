23205d5ec5e9dc316aaa9c8238cfbc87de1268ec
Pablo Lorenzo <apjlorenzo1@gmail.com>
2024-08-27 21:22:32 -0600

    Menu Principal

---
+++ Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_1
+{
+    public class Menu
+    {
+        public void MenuPrincipal()
+        {
+            Console.Clear();
+            Console.WriteLine("___________________________________");
+            Console.WriteLine("::::::::::MENU::::::::::");
+            Console.WriteLine("1.REGISTRAR VEHÍCULOS");
+            Console.WriteLine("2.RETIRAR VEHÍCULOS");
+            Console.WriteLine("3.MOSTRAR VEHÍCULOS ESTACIONADOS");
+            Console.WriteLine("4.MOSTRAR ESPACIOS DISPONIBLES");
+            Console.WriteLine("5.SALIR");
+            Console.WriteLine("___________________________________");
+            Console.Write("Ingrese una opción: ");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Proyecto_1;
+
+Menu menu = new Menu();
+bool condicionUno = true;
+try
+{
+    do
+    {
+        menu.MenuPrincipal();
+        try
+        {
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    condicionUno = false;
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Clear();
+            Console.WriteLine(ex.Message);
+            Console.ReadKey();
+        }
+
+    }
+    while (condicionUno);
+
+}
+finally
+{
+    Console.WriteLine("\nGracias por utilizar nuestro sistema...");
+}
